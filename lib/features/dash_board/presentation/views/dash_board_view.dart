a811a448ab3ba2cc3cd7b2fbbfdd36346bcf815e
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2025-06-14 14:46:59 +0300

    Add responsive layout components for dashboard: implement SizeConfig, mobile and tablet layouts, and enhance desktop layout structure

--- lib/features/dash_board/presentation/views/dash_board_view.dart
+++ lib/features/dash_board/presentation/views/dash_board_view.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/core/utils/size_config.dart';
 import 'package:responsive_dash_board/core/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dash_board/features/dash_board/presentation/views/widgets/custom_drawer.dart';
 import 'package:responsive_dash_board/features/dash_board/presentation/views/widgets/dash_board_desktop_layout.dart';
+import 'package:responsive_dash_board/features/dash_board/presentation/views/widgets/dash_board_mobile_layout.dart';
+import 'package:responsive_dash_board/features/dash_board/presentation/views/widgets/dashboard_tablet_layout.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig.init(context);
+
     return Scaffold(
+      appBar: MediaQuery.sizeOf(context).width < SizeConfig.tablet ? dashBoardViewAppBar() : null,
       backgroundColor: Color(0XFFF7F9FA),
+      drawer: MediaQuery.sizeOf(context).width < SizeConfig.tablet
+          ? Drawer(
+              width: MediaQuery.sizeOf(context).width * 0.6,
+              child: CustomDrawer(),
+            )
+          : null,
       body: AdaptiveLayoutWidget(
-        mobileLayout: (context) => const SizedBox.shrink(),
-        tabletLayout: (context) => const SizedBox.shrink(),
-        desktopLayout: (context) => DashBoardDesktopLayout(),
+        mobileLayout: (context) => const DashBoardMobileLayout(),
+        tabletLayout: (context) => const DashboardTabletLayout(),
+        desktopLayout: (context) => const DashBoardDesktopLayout(),
+      ),
+    );
+  }
+
+  AppBar dashBoardViewAppBar() {
+    return AppBar(
+      backgroundColor: Color(0xFFFAFAFA),
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      leading: Builder(
+        builder: (context) => IconButton(
+          onPressed: () => Scaffold.of(context).openDrawer(),
+          icon: const Icon(Icons.menu),
+        ),
       ),
     );
   }
